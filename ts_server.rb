cee3e871b0ab6d19741335d1cb724eec2f22a05f
Mike Andrianov <mikeandrianov@gmail.com>
2013-05-24 19:25:50 +0300

    init commit

---
+++ ts_server.rb
@@ -0,0 +1,22 @@
+require 'drb/drb'
+class FactServer
+	def initialize(ts)
+		@ts = ts
+	end
+	def main_loop
+		loop do
+			tuple = @ts.take(['fact', Integer, Integer])
+			m = tuple[1]
+			n = tuple[2]
+			value = (m..n).inject(1) { |a, b| a * b }
+
+			puts "server works... with #{m}-#{n} range"
+
+			@ts.write(['fact-answer', m, n, value])
+		end
+	end
+end
+ts_uri = ARGV.shift || 'druby://localhost:12345'
+DRb.start_service
+$ts = DRbObject.new_with_uri(ts_uri)
+FactServer.new($ts).main_loop
